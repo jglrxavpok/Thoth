7cc51859158b0a0068fab64dd0cbf8af3c7a28d6
jglrxavpok <jglrxavpok@gmail.com>
2015-05-23 12:04:56 +0200

    Added some documentation

--- src/main/java/org/jglr/thoth/BinUtils.java
+++ src/main/java/org/jglr/thoth/BinUtils.java
@@ -2,6 +2,15 @@ package org.jglr.thoth;
 
 public class BinUtils {
 
+    /**
+     * Checks if <code>value</code> has given flag
+     * @param value
+     *      The value to check
+     * @param flag
+     *      The flag to search
+     * @return
+     *      <code>true</code> if <code>value</code> has given flag, <code>false</code> otherwise
+     */
     public static boolean hasFlag(int value, int flag) {
         return (value & flag) != 0;
     }
--- src/main/java/org/jglr/thoth/ThothInterpreter.java
+++ src/main/java/org/jglr/thoth/ThothInterpreter.java
@@ -13,10 +13,19 @@ public class ThothInterpreter {
         this.state = new InterpreterState();
     }
 
+    /**
+     * Executes <code>function</code> and returns the result.
+     * @param function
+     *      The function to execute
+     * @param params
+     *      The values given as parameters
+     * @return
+     *      The result of the function call
+     */
     public String interpret(ThothFunc function, ThothValue... params) {
         ThothValue[] actualParams = new ThothValue[function.getArgsNumber()];
         int min = Math.min(actualParams.length,params.length);
-        for(int i = 0;i<min;i++) {
+        for(int i = 0;i<min;i++) { // Try to fill the actualParams array with the given parameters
             if (params[i] != null) {
                 actualParams[i] = params[i];
             } else {
@@ -24,7 +33,7 @@ public class ThothInterpreter {
             }
         }
 
-        for(int i = min;i<actualParams.length;i++) {
+        for(int i = min;i<actualParams.length;i++) { // Finish filling the actualParams array with nulls if needed
             actualParams[i] = new NullValue();
         }
 
@@ -37,11 +46,17 @@ public class ThothInterpreter {
                 throw new RuntimeException("Tried to jump to wrong label: "+state.label);
             }
             ThothCommandment insn = insns.get(state.insnPointer);
-            builder.append(insn.execute(actualParams, state, this));
+            String partialResult = insn.execute(actualParams, state, this);
+            builder.append(partialResult);
         }
         return builder.toString();
     }
 
+    /**
+     * Generates the destination to which jump for every label instruction
+     * @param insns
+     *      The list of instructions in which to search for labels
+     */
     private void computeJumpsDestinations(List<ThothCommandment> insns) {
         for(int i = 0;i<insns.size();i++) {
             ThothCommandment insn = insns.get(i);
@@ -53,6 +68,11 @@ public class ThothInterpreter {
         }
     }
 
+    /**
+     * Jump to the given destination
+     * @param destination
+     *      The label to which jump
+     */
     public void jump(String destination) {
         state.insnPointer = state.getJumpLocation(destination);
         state.label = destination;
